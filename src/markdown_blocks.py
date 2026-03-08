2d1db74411dee250b8c64da2a14aadc86670023e
nielbarasona <niel.j.barasona@gmail.com>
2026-03-08 00:03:33 -0500

    Add markdown_to_html_node

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,9 @@
 from enum import Enum
+from htmlnode import ParentNode
 import re
+
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class BlockType(Enum):
@@ -47,3 +51,66 @@ def block_to_block_type(block):
         return BlockType.OLIST
 
     return BlockType.PARAGRAPH
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    nodes = []
+    for block in blocks:
+        match block_to_block_type(block):
+            case BlockType.HEADING:
+                counter = 0
+                while block[counter] == "#":
+                    counter += 1
+                content = block[counter:].strip()
+                children = text_to_children(content)
+                heading_node = ParentNode(f"h{counter}", children)
+                nodes.append(heading_node)
+            case BlockType.CODE:
+                content = block[4:-3]
+                text_node = TextNode(content, TextType.TEXT)
+                content_leaf = text_node_to_html_node(text_node)
+                code_parent = ParentNode("code", [content_leaf])
+                pre_parent = ParentNode("pre", [code_parent])
+                nodes.append(pre_parent)
+            case BlockType.QUOTE:
+                lines = block.split("\n")
+                new_lines = []
+                for line in lines:
+                    new_lines.append(line[1:].strip())
+                rejoined = " ".join(new_lines)
+                children = text_to_children(rejoined)
+                quote_node = ParentNode("blockquote", children)
+                nodes.append(quote_node)
+            case BlockType.ULIST:
+                lines = block.split("\n")
+                children = []
+                for line in lines:
+                    stripped = line[2:].strip()
+                    children.append(ParentNode("li", text_to_children(stripped)))
+                ulist_node = ParentNode("ul", children)
+                nodes.append(ulist_node)
+            case BlockType.OLIST:
+                lines = block.split("\n")
+                children = []
+                for line in lines:
+                    stripped = line.split(". ", 1)[1].strip()
+                    children.append(ParentNode("li", text_to_children(stripped)))
+                olist_node = ParentNode("ol", children)
+                nodes.append(olist_node)
+            case _:
+                lines = block.split("\n")
+                rejoined = " ".join(lines)
+                children = text_to_children(rejoined)
+                paragraph_node = ParentNode("p", children)
+                nodes.append(paragraph_node)
+    parent_node = ParentNode("div", nodes)
+    return parent_node
+
+
+def text_to_children(text):
+    textnodes = text_to_textnodes(text)
+    nodes = []
+    for node in textnodes:
+        nodes.append(text_node_to_html_node(node))
+    return nodes
